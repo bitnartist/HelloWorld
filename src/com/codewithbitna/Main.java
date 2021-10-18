53dd3ab6a0cf90f26ec3083c79a901b4bf677f60
sophia.swengineer <sophia.swengineer@gmail.com>
2021-10-18 16:23:29 +0900

    Initial commit

---
+++ src/com/codewithbitna/Main.java
@@ -0,0 +1,44 @@
+package com.codewithbitna;
+
+import java.text.NumberFormat;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("four times table");
+        int result = 4*1;
+        System.out.println(result);
+        result = 4 * 2;
+        System.out.println(result);
+        result = 4 * 3;
+        System.out.println(result);
+        result = 4 * 4;
+        System.out.println(result);
+        result = 4 * 5;
+        System.out.println(result);
+        result = 4 * 6;
+        System.out.println(result);
+        result = 4 * 7;
+        System.out.println(result);
+        result = 4 * 8;
+        System.out.println(result);
+        result = 4 * 9;
+        System.out.println(result);
+
+        System.out.println("What time table is this: ");
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
+        System.out.println(number * 1);
+        System.out.println(number * 2);
+        System.out.println(number * 3);
+        System.out.println(number * 4);
+        System.out.println(number * 5);
+        System.out.println(number * 6);
+        System.out.println(number * 7);
+        System.out.println(number * 8);
+        System.out.println(number * 9);
+
+    }
+}
